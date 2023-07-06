2779cb776c3b36dbd153e3839268e91050511241
Vlad <efremov.vlad.ser@gmail.com>
2023-07-06 16:34:28 +0600

    feat: add tabs

---
+++ app.js
@@ -0,0 +1,22 @@
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(function(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        tabsBtn.forEach(function(item) {
+            item.classList.remove('active');
+        });
+
+        tabsItems.forEach(function(item) {
+            item.classList.remove('active');
+        });
+
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+        
+    });
+});
